58fe3398f35bd51f5bb1e239eafcfe5032512bfb
Stu-Gotz <gamer691@gmail.com>
2021-05-21 19:10:14 +0100

    pushing to pull

--- main.js
+++ main.js
@@ -5,17 +5,6 @@ DOMStrings = {
     btn3: document.querySelector('.btn__main--3'),
 }
 
-// const printLetters = (target, message, speed) => {
-//     let i = 0;
-//     const interval = setInterval(() => {
-//         document.querySelector(target).innerHTML += message.charAt(i);
-//         i++;
-//         if (i > message.length) {
-//             clearInterval(interval);
-//         }
-//     }, speed);
-// };
-
 const clearSection = () => {
 
     DOMStrings.main.innerHTML = "";
@@ -24,8 +13,7 @@ const clearSection = () => {
 const insertAbout = () => {
 
     const markup = `
-    <div class="about__text">I am an adaptive person looking to further my career in software development. I have spent the last year learning and applying technical knowledge to accomplish tasks at work, particularly using Python to make API calls to services, web-scraping, cleaning, storing and displaying data. I have thoroughly enjoyed the challenge and have under taken learning Sass/CSS, HTML, and nodeJS/Javascript. I am eager to begin a new challenge and apply myself to new, challenging and different problems.
-    <br><br>Life happened and I am losing my contracted position, so I made this page in an effort to prove competency and hopefully land a job. I know it's not world-beating, but I made it in about a day because, as my boss says "don't let perfect the enemy of good". I have other things to work on, which will be reference.</div>
+    <div class="about__text">I am an adaptive person looking to further my career in software development. I have spent the last year learning and applying technical knowledge to accomplish tasks at work, particularly using Python to make API calls to services, web-scraping, cleaning, storing and displaying data. I have thoroughly enjoyed the challenge and have under taken learning Sass/CSS, HTML, and nodeJS/Javascript. I am eager to begin a new challenge and apply myself to new, challenging and different problems.</div>
     `;
 
     DOMStrings.main.insertAdjacentHTML('afterbegin', markup);
@@ -38,6 +26,7 @@ const insertTech = () => {
         <li>CSS & Sass</li>
         <li>HTML</li>
         <li>Javascript (ES5/6)</li>
+        <
         <li>Python 3.x</li>
         <li>PostgreSQL</li>
     </ul>
@@ -52,7 +41,6 @@ const insertHire = () => {
     <div class="hire__text"> I'm cheap, eager to learn, driven by curiosity and the desire to grow rather than money. I believe in minimal design, and simple, easy to navigate layouts. Having worked for the government and dealt with what can be described using diplomatic language as "horrible websites" I want to make the internet a nicer place, with prettier pages.</div>
     `;
 
-    // printLetters('.about', markup, 30);
     DOMStrings.main.insertAdjacentHTML('afterbegin', markup);
 }
 
